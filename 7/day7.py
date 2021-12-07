068510e3ec49aaedd52ae3a3e0189ae70930e8da
stijnstroeve <stijnstroeve@hotmail.com>
2021-12-07 07:21:48 +0100

    Added day 7 of AoC 2021.

---
+++ 7/day7.py
@@ -0,0 +1,33 @@
+import math
+
+file = open('./7/input.txt', 'r')
+input = [int(x) for x in file.read().split(',')]
+
+def part1():
+    min_fuel = math.inf
+    for target_x in range(1, max(input)):
+        sum = 0
+        for current_x in input:
+            sum += abs(target_x - current_x)
+
+        if sum < min_fuel:
+            min_fuel = sum
+
+    return min_fuel
+    
+def part2():
+    min_fuel = math.inf
+    for target_x in range(1, max(input)):
+        sum = 0
+        for current_x in input:
+            delta = abs(target_x - current_x)
+            triangle_number = int((delta ** 2 + delta) / 2)
+            sum += triangle_number
+
+        if sum < min_fuel:
+            min_fuel = sum
+
+    return min_fuel
+
+print("Part1:", part1())
+print("Part2:", part2())
